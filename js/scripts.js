3b40d45cfd59564d27efd5db640af6a86a817114
marjsky <67877013+marjsky@users.noreply.github.com>
2022-05-13 22:05:26 -0400

    added objects in array of  pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-
+let pokemonList = [
+    {
+        name: 'Beedrill',
+        height: 1,
+        types: ['bug', 'poison'],
+        abilities: ['Swarm', 'Sniper']
+    },
+    {
+        name: 'Nidoqueen',
+        height: 1.3,
+        types: ['ground', 'poison'],
+        abilities: ['Poison-point', 'Rivalry', 'Sheer-force']
+    },
+    {
+        name: 'Arcanine',
+        height: 1.9,
+        types: 'fire',
+        abilities: ['Flash-fire', 'Intimidate', 'Justified']
+    },
+    {
+        name: 'Pansear',
+        height: 0.6,
+        types: 'fire',
+        abilities: ['Blaze', 'Gluttony']
+    }
+];
