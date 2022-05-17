7cd42e02565d031939c9d327aa8f6d01090a7b47
marjsky <67877013+marjsky@users.noreply.github.com>
2022-05-16 20:50:12 -0400

    established conditional at special value

--- js/scripts.js
+++ js/scripts.js
@@ -30,4 +30,11 @@ for (let i=0; i < pokemonList.length; i++) {
      
     //rendering in display each name and height through counter as index
     document.write(pokemonList[i].name + " (height " + pokemonList[i].height + "m)");
+
+    // set conditon highest value of height from the list
+    if (pokemonList[i].height == 1.9) {
+        document.write(" - Wow, that's huge!<br>");
+    } else {
+        document.write("<br>");
+    }
 }
