8c36e9bca18fc7f9958334f972aa0a295c8745dd
marjsky <67877013+marjsky@users.noreply.github.com>
2022-06-06 06:11:19 -0400

    create UI pattern of modal display

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let pokemonRepository = (function (){
     // empty array that will push in from API and display it
     let pokemonList = [];
     let apiurl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let modalContainer = document.querySelector('#modal-container');
 
     // adding pokemon with validate data
     function add (item) {
@@ -60,7 +61,7 @@ let pokemonRepository = (function (){
         return fetch(url).then(function(response) {
             return response.json();
         }).then(function (details) {
-            item.imageUrl = details.sprites.font_default;
+            item.imageUrl = details.sprites.other.dream_world.front_default;
             item.height = details.height;
             item.types = details.types;
         }).catch(function (e) {
@@ -68,12 +69,68 @@ let pokemonRepository = (function (){
         });
     }
 
-    // reveal API's object in console clicked button
+    // reveal API's object in modal display from clicked button
     function showDetails(item) {
         pokemonRepository.loadDetails(item).then(function () {
-            console.log(item);
+            showModal(item);
         });
     }
+
+    // UI pattern of modal display
+    function showModal(item) {
+        // clear all existing model content
+        modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        // add the new modal content
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        // add h1 element as name data to the modal
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = item.name;
+
+        // add p element as height data to the modal
+        let contentElement = document.createElement('p');
+        contentElement.innerText = `Height: ${item.height} m`;
+
+        // add img element as svg to the modal
+        let imageElement = document.createElement('img');
+        imageElement.src = item.imageUrl;
+
+        // these elements for modal attach as child to div.modal
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(imageElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+    }
+
+    // to close down model
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    }
+
+    // Press Escape keydown as function to call hideout to close model
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' &&
+        modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    })
+
+    // click outside of modal display to close modal
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
 
     // IIFE returns object contains methods that reference functions.
     return {
